e05e2c0379f7ba918c6d8bae5b4bd9abf9dbaa7c
dido18 <davideneri18@gmail.com>
2016-06-23 12:44:40 +0200

    chane control to log

--- src/stresscentrality/LogMessages.java
+++ src/stresscentrality/LogMessages.java
@@ -5,46 +5,39 @@ import peersim.core.Control;
 import peersim.core.Network;
 import peersim.core.Node;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 
-public class ControlStressC implements Control {
+public class LogMessages implements Control {
 
 	private static final String FILE_PATH = "filepath";
 	private final String pathFile ;
 
-	public ControlStressC(String prefix){
+	public LogMessages(String prefix){
         pathFile = Configuration.getString(prefix+"."+FILE_PATH);
-		SimpleDateFormat time_formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		String current_time_str = time_formatter.format(System.currentTimeMillis());
-		writeIntoFile("\n"+ current_time_str+" - ");
+        SimpleDateFormat time_formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String current_time_str = time_formatter.format(System.currentTimeMillis());
+        writeIntoFile("\n"+ current_time_str+" - ");
 
 	}
 
 	public boolean execute() {
 
-		long sumSCs=0; ///sum of the
 		long cycle = 0L;
+		long numNosp=0L;
+		long numControl=0L;
 		for(int n =0; n < Network.size(); n++) {
             Node v = Network.get(n);
 			ReportPhase rp = ((ReportPhase) v.getProtocol(2));
 			CountPhase cp = ((CountPhase) v.getProtocol(1));
-            long tempTotal = 0;
-			for (int i = 0; i < Network.size(); i++) {
-				for (int j = 0; j < Network.size(); j++) {
-                    Node s = Network.get(i);
-                    Node t = Network.get(j);
-					if (rp.tableReport.contains(s,t)){
-                        int sv =  cp.spTable.get(s);
-                        int vt =  cp.spTable.get(t);
-						tempTotal += sv*vt;
-                        }
-				}
-			}
-			sumSCs +=tempTotal;
+			numNosp += cp.numNOSP;
+			numControl += rp.numReport;
 			cycle = cp.cycle;
 		}
-		String log = "("+cycle+","+sumSCs+")";
+		String log = "("+cycle+","+numNosp+numControl+")";
 
 		writeIntoFile(log);
 
